9a935ae1615f4a76b46466b9386bbcc24c24f292
Hyun Kim <hkim@live.com.au>
2019-07-13 09:27:33 +1000

    revert

--- db.js
+++ db.js
@@ -1,14 +1,5 @@
 import * as firebase from 'firebase';
-
-const dbConfig = {
-  apiKey: 'AIzaSyDOj4HOjme4t0FcS_tH6Pts7QLA3YT34-Y',
-  authDomain: 'agspine-2847c.firebaseapp.com',
-  databaseURL: 'https://agspine-2847c.firebaseio.com',
-  projectId: 'agspine-2847c',
-  storageBucket: 'agspine-2847c.appspot.com',
-  messagingSenderId: '492799843537',
-  appId: '1:492799843537:web:7e112c2fd9973c4e',
-};
+import { dbConfig } from './env';
 
 const DB = config => () => {
   return firebase.initializeApp(config);
