ef617ee9a9c49b400de1c0fe67a0a4f0a84b6f87
Hyun Kim <hkim@live.com.au>
2019-07-13 10:51:09 +1000

    ill refactor late

--- constants/Colors.js
+++ constants/Colors.js
@@ -4,4 +4,29 @@ const PRIMARY = {
   500: '#289124',
 };
 
+const buttonColors = {
+  // Green
+  PRIMARY_BUTTON_COLOR: '#64B3AB',
+
+  // WHITE
+  SECONDARY_BUTTON_COLOR: '#FFF',
+};
+
+const colors = {
+  // Heavy orange
+  PRIMARY_COLOR: '#DB8946',
+  // Dark black
+  SECONDARY_COLOR: '#2C2C2C',
+  // Light yella
+  SHADED_PRIMARY: '#FAECCC',
+  //
+  PRIMARY_TEXT_COLOR: '#F3B85B',
+  // White
+  SECONDARY_TEXT_COLOR: '#FFF',
+};
+
 export { PRIMARY };
+export default {
+  ...colors,
+  ...buttonColors,
+};
