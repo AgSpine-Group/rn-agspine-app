5e4ea433759b11b0151d4c3eb2ec1bbdf2c78063
hyun <hkim@live.com.au>
2019-07-22 05:32:34 +1000

    signup and register no google auth

--- firebase.js
+++ firebase.js
@@ -3,6 +3,8 @@ import 'firebase/firestore';
 import * as env from './env';
 
 const fb = config => () => {
+  console.disableYellowBox = true
+  console.reportErrorsAsExceptions = false;
   return firebase.initializeApp(config);
 };
 
