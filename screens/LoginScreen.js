8d96eff4df38ed6c86fc150010a68745c1868fe5
Hyun Kim <hkim@live.com.au>
2019-07-21 22:18:58 +1000

    Signup/register (#9)
    * signup and register no google auth
    
    * remove lauch json
    
    * another one
    
    * IM A MUPPET

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,13 +7,24 @@ import {
   Container,
   Header,
   Content,
+  Card,
+  Icon,
+  Button,
   Form,
-  Item as FormItem,
+  Item,
   Label,
 } from 'native-base';
 import { StyleSheet } from 'react-native';
 import { PrimaryButton, SecondaryButton } from '../components/Button';
 import colors from '../constants/Colors';
+import { PRIMARY, GREY } from '../constants/Colors';
+
+import { ImageBackground } from 'react-native'
+
+import { Dimensions, Animated, Switch } from "react-native";
+
+var width = Dimensions.get('window').width; //full width
+var height = Dimensions.get('window').height; //full height
 
 const styles = StyleSheet.create({
   container: {
@@ -52,12 +63,144 @@ const fixedContainer = {
   alignItems: 'center',
 };
 
+const loginStyles = {
+  background: {
+    backgroundColor: 'red',
+    position: 'relative'
+  },
+  card: {
+    padding: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    backgroundColor: 'white',
+    width: width,
+    position: 'absolute',
+  },
+  horizontal: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  primaryButton: {
+    marginTop: 40,
+    shadowOpacity: 0,
+    elevation: 0,
+    backgroundColor: PRIMARY[400]
+  },
+  primaryText: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  outlineButton: {
+    marginTop: 5,
+    backgroundColor: '#fff',
+    borderColor: PRIMARY[800],
+    borderRadius: 1
+  },
+  outlineButtonText: {
+    color: PRIMARY[600],
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  inactiveIcon: {
+    color: PRIMARY[200]
+  },
+  inactiveInputText: {
+    color: GREY[600]
+  },
+  forgotPasswordContainer: {
+    display: 'flex',
+    marginTop: 20,
+    alignItems: 'flex-end',
+    width: '100%'
+  },
+  forgotPasswordText: {
+    color: PRIMARY[400],
+    fontSize: 14
+  },
+  spacer: {
+    margin: 40,
+  },
+  toggle: {
+    container: {
+      display: 'flex',
+      flexDirection: 'row',
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'flex-end'
+    },
+    inactiveText: {
+      fontSize: 12,
+      color: GREY[600]
+    },
+    activeText: {
+      fontSize: 12,
+      color: PRIMARY[800]
+    }
+  },
+  frontScreen: {
+    container: {
+      display: 'flex',
+      height,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    textContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    body: {
+      container: {
+        width: width * 0.8,
+        padding: 20,
+      },
+      text: {
+        color: '#fff',
+        textAlign: 'center'
+      }
+    },
+    title: {
+      color: '#fff',
+      fontWeight: 'bold',
+      fontSize: 36
+    },
+    buttonContainer: {
+      display: 'flex',
+      width: width * 0.8,
+    },
+    registerButton: {
+      backgroundColor: '#fff',
+    },
+    registerText: {
+      fontWeight: 'bold',
+      fontSize: 16,
+      color: PRIMARY[800]
+    },
+    loginButton: {
+      marginTop: 10,
+      backgroundColor: PRIMARY[400],
+    },
+    loginText: {
+      fontWeight: 'bold',
+      fontSize: 16
+    }
+  },
+}
+
+function applyLetterSpacing(string, count = 1) {
+  return string.split('').join('\u200A'.repeat(count));
+}
+
 export default class LoginScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       password: '',
       email: '',
+      showForm: false,
+      type: '',
+      bottom: new Animated.Value(-1000),
     };
   }
 
@@ -101,55 +244,109 @@ export default class LoginScreen extends React.Component {
     }
   };
 
+  showForm = type => () => {
+    this.setState({
+      showForm: true,
+      type
+    })
+    Animated.timing(this.state.bottom, {
+      toValue: 0,
+      duration: 500
+    }).start();
+  }
+
+  toggle = () => {
+    const currentValue = this.state.type;
+
+    const oppositeValue = currentValue === 'sign-up' ? 'log-in' : 'sign-up';
+
+    this.setState({
+      type: oppositeValue
+    })
+  }
+
   render() {
     return (
-      <Container>
-        <Header center="Hi there" />
-        <Content style={styles.yellow} contentContainerStyle={fixedContainer}>
-          <View style={styles.innerContainer}>
-            <Text style={styles.heading}>Login here</Text>
-            <View style={styles.container}>
-              <Form style={styles.formWrapper}>
-                <FormItem floatingLabel>
-                  <Label>Email</Label>
-                  <Input
-                    tag="email"
-                    type="email"
-                    onChangeText={this.handleChange('email')}
-                    value={this.state.email}
-                    textContentType="emailAddress"
-                  />
-                </FormItem>
-                <FormItem floatingLabel>
-                  <Label>Password</Label>
-                  <Input
-                    tag="password"
-                    type="password"
-                    onChangeText={this.handleChange('password')}
-                    value={this.state.password}
-                    textContentType="password"
-                    secureTextEntry
-                  />
-                </FormItem>
-              </Form>
-              <PrimaryButton onPress={this.handleLogin} full primary style={{ marginTop: 35 }}>
-                <Text>Login</Text>
-              </PrimaryButton>
-
-              <SecondaryButton
-                onPress={this.handleSignup}
-                secureTextEntry
-                full
-                light
-                primary
-                style={{ marginTop: 35 }}
-              >
-                <Text>Sign up</Text>
-              </SecondaryButton>
+      <ImageBackground
+        source={require('../assets/images/background-2.jpg')}
+        style={{ width: '100%', height: '100%', position: 'relative' }}
+      >
+        {
+          !this.state.showForm && (
+            <View style={loginStyles.frontScreen.container}>
+              <View style={loginStyles.frontScreen.textContainer}>
+                <Text style={loginStyles.frontScreen.title}>{
+                  applyLetterSpacing('AGSPINE')
+                }</Text>
+                <View style={loginStyles.frontScreen.body.container}>
+                  <Text style={loginStyles.frontScreen.body.text}>Digitalise and streamline your agriculture business</Text>
+                </View>
+              </View>
+              <View style={loginStyles.frontScreen.buttonContainer}>
+                <Button style={loginStyles.frontScreen.registerButton} full onPress={this.showForm('sign-up')}>
+                  <Text style={loginStyles.frontScreen.registerText}>Sign Up</Text>
+                </Button>
+                <Button style={loginStyles.frontScreen.loginButton} full onPress={this.showForm('log-in')}>
+                  <Text style={loginStyles.frontScreen.loginText}>Log In</Text>
+                </Button>
+              </View>
             </View>
+          )
+        }
+        <Animated.View style={Object.assign({
+          bottom: this.state.bottom
+        }, loginStyles.card)}>
+          <View style={loginStyles.horizontal}>
+            <View style={loginStyles.toggle.container}>
+              <Text style={this.state.type === 'sign-up' ? loginStyles.toggle.activeText : loginStyles.toggle.inactiveText}>Sign Up</Text>
+              <Switch
+                onChange={this.toggle}
+                onValueChange={this.toggle}
+                value={this.state.type === 'log-in'}
+              />
+              <Text style={this.state.type === 'log-in' ? loginStyles.toggle.activeText : loginStyles.toggle.inactiveText}>Log In</Text>
+            </View>
+            <Form>
+              <Item floatingLabel>
+                <Label style={loginStyles.inactiveInputText}>Email</Label>
+                <Icon name='email' type='MaterialIcons' style={loginStyles.inactiveIcon} />
+                <Input
+                  tag="email"
+                  type="email"
+                  onChangeText={this.handleChange('email')}
+                  value={this.state.email}
+                  textContentType="emailAddress"
+                />
+              </Item>
+              <Item floatingLabel>
+                <Label style={loginStyles.inactiveInputText}>Password</Label>
+                <Icon name='eye' type='Entypo' style={loginStyles.inactiveIcon} />
+                <Input
+                  tag="password"
+                  type="password"
+                  onChangeText={this.handleChange('password')}
+                  value={this.state.password}
+                  textContentType="password"
+                  secureTextEntry
+                />
+              </Item>
+              <View style={loginStyles.forgotPasswordContainer}>
+                <Text style={loginStyles.forgotPasswordText}>Forgot your password?</Text>
+              </View>
+              <Button style={loginStyles.primaryButton} full onPress={this.state.type === 'sign-up' ? this.handleSignup : this.handleLogin}>
+                <Text style={loginStyles.primaryText}>{this.state.type === 'sign-up' ? 'Sign' : 'Log'} In</Text>
+              </Button>
+              <Button iconLeft bordered style={loginStyles.outlineButton} full>
+                <Icon name='google' type='AntDesign' style={loginStyles.outlineButtonText} />
+                <Text style={loginStyles.outlineButtonText}>{this.state.type === 'sign-up' ? 'Sign' : 'Log'} In With google</Text>
+              </Button>
+
+              <View style={loginStyles.spacer}>
+              </View>
+            </Form>
           </View>
-        </Content>
-      </Container>
+        </Animated.View>
+      </ImageBackground >
     );
   }
 }
